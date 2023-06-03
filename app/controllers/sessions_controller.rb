58c6a3a73de5c84d8d942bddfefc785b8aaeac62
yuta <calendula6724yuta@gmail.com>
2023-06-03 13:53:06 +0900

    セッション完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required, only: %i[new create]
   def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = "失敗"
+      render :new
+    end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = "ログアウト"
+    redirect_to new_session_path
   end
 end
