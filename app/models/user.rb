c8025235a12fb5f23d98d84b843a38107f42e6be
yuta <calendula6724yuta@gmail.com>
2023-06-03 13:11:53 +0900

    ログイン画面作成　sessionまだ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates :name, persence: true, length: { maxmum: 30 }
-  validates :email, presence: true, length: { maxmum: 255 }
-  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, lenth: { minmum: 6 }
+  validates :password, length: { minimum: 6 }
 end
 
