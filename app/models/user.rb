c5a1060ef84c68bfed50bd31366a377590ffaa0c
yuta <calendula6724yuta@gmail.com>
2023-06-03 12:27:07 +0900

    usersコントローラ作成　user.rb OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   validates :name, persence: true, length: { maxmum: 30 }
   validates :email, presence: true, length: { maxmum: 255 }
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, lenth: { minmum: 6 }
 end
 
