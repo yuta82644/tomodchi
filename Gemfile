e471ee0863830aac09d25d08cc4d9518ac05e37d
yuta <calendula6724yuta@gmail.com>
2023-06-03 16:24:00 +0900

    feedscontrollerindex new制作

--- Gemfile
+++ Gemfile
@@ -29,7 +29,9 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
-
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,10 +5,52 @@ class FeedsController < ApplicationController
 
   def new
     @feed = Feed.new
-    
   end
-end
 
-def show
-  
+  def create
+    @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
+    if params[:back]
+      render :new
+    else
+      if @feed.save
+        redirect_to feeds_path, notice: "投稿制作されました"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+    @feed = Feed.find(params[:id])
+  end
+
+  def edit
+    @feed = Feed.find(params[:id])
+  end
+
+  def update
+    @feed = Feed.find(params[:id])
+    if @feed.update(feed_params)
+      redirect_to feeds_path, notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+  def confirm
+    @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
+  end
+
+  def destroy
+    @feed = Feed.find(params[:id])
+    @feed.destroy
+    redirect_to feeds_path, notice: "削除しました"
+  end
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:image, :image_cache)
+  end
 end
