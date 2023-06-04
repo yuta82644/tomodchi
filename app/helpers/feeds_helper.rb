dbe73a26d0043d47c333a77ede12195fc8ed01dd
yuta <calendula6724yuta@gmail.com>
2023-06-04 19:07:57 +0900

    edit画面修正

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,5 +1,6 @@
 module FeedsHelper
-  def choose_new_or_edit
+
+  def choose_new_or_edit(feed)
     if action_name == "new" || action_name == 'create'
       confirm_feeds_path
     elsif action_name == 'edit'
